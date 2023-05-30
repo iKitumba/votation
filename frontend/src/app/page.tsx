2794884c371ccd0b09ca2976c99a94d78523a4d7
Alberto Kitumba <alberkitumba@gmail.com>
2023-05-30 08:02:30 +0100

    Add Scaffold

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,95 +1,48 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import { Footer } from "@/components/Footer";
+import styles from "./page.module.css";
+import { Header } from "@/components/Header";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <div className={styles.homeContainer}>
+      <section className={styles.presentationSection}>
+        <article className={styles.bannerSection}>
+          <div className={styles.bannerSectionLeft}>
+            <h1 className={styles.bannerSectionLeftTitle}>Votação</h1>
+            <p className={styles.bannerSectionLeftDesc}>
+              Uma <strong>forma</strong> antiga de <strong>resolver</strong>{" "}
+              novos <strong>problemas</strong>
+            </p>
+          </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+          <img src="/vote-illustration.svg" alt="Votação" />
+        </article>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+        <section className={styles.pilaresContainer}>
+          <article className={styles.pilarContainer}>
+            <div className={styles.pilarText}>
+              Acreditamos que o pilar de uma sociedade igualitária seja uma onde
+              todos possam decidir o que é melhor para todos
+            </div>
+            <div className={styles.pilarLugar}>1º</div>
+          </article>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+          <article className={styles.pilarContainer}>
+            <div className={styles.pilarText}>
+              Acreditamos que o pilar de uma sociedade igualitária seja uma onde
+              todos possam decidir o que é melhor para todos
+            </div>
+            <div className={styles.pilarLugar}>2º</div>
+          </article>
+          <article className={styles.pilarContainer}>
+            <div className={styles.pilarText}>
+              Acreditamos que o pilar de uma sociedade igualitária seja uma onde
+              todos possam decidir o que é melhor para todos
+            </div>
+            <div className={styles.pilarLugar}>3º</div>
+          </article>
+        </section>
+      </section>
+    </div>
+  );
 }
